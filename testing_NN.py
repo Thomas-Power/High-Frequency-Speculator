f1c2923a467ec64a08739cd88dbc7addd5753136
Thomas Power <tpaschalpower@gmail.com>
2018-06-17 22:00:36 +0100

    Add files via upload

--- testing_NN.py
+++ testing_NN.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 from tensorflow.python.framework import ops
 from sklearn.preprocessing import MinMaxScaler
 import random
@@ -25,30 +26,35 @@ Projection_Value = np.ones([30], dtype=float)
 Projection_Time = np.ones([30], dtype=float)
 Hist_Source = np.ones([30], dtype=float)
 Hist_Time = np.ones([30], dtype=float)
-batch_x = np.ones([6, 30], dtype=float)
-batch_y = np.ones([6], dtype=float)
+batch_x = np.ones([len(Source_Data)-13, 30], dtype=float)
+batch_y = np.ones([len(Source_Data)-13], dtype=float)
+preditions = np.ones([len(Source_Data)-13], dtype=float)
 
 #Create batch_pool. Data to be fed into neural network
-for i in range(6):
-	T = random.randint(30, len(Source_Data)-13)		# pick random point for T
+for i in range(40, (len(Source_Data)-13)):
+	T = i
 	T_Vector = Source_Data[T]						# Get vector for T
 	Hist_Price = Source_Data[(T-30) : T]			# gather 30 points before T as Historical data
-	Target = Source_Data[T + (i+6)]					# randomly pick T + [6-12] as target value
+	Target = Source_Data[T + 9]						# randomly pick T + [6-12] as target value
 	for e in range(30):
 		Projection_Value[e] = Hist_Price[e]
 		
 	batch_x[i] = Projection_Value # projections and target now defined as batch_pool[i]
 	batch_y[i] = Target
-
+	
 saver = tf.train.import_meta_graph('./temp/trained_model-1000.meta')
 with tf.Session() as sess:
 	saver.restore(sess,tf.train.latest_checkpoint('./temp'))
 	graph = tf.get_default_graph()
 	Y_ = graph.get_tensor_by_name('Output:0')
 	X = graph.get_tensor_by_name('InputData:0')
-	print("Estimates:")
-	print(scaler.inverse_transform(sess.run(Y_, feed_dict={X: batch_x})))
-	print("Actual:")
-	for i in range(6):
-		print(scaler.inverse_transform(batch_y[i]))
-		
+	
+	preditions = sess.run(Y_, feed_dict={X: batch_x})
+
+preditions = scaler.inverse_transform(preditions)
+batch_y = scaler.inverse_transform(Source_Data)
+	
+plt.plot(preditions, label="Prediction")
+plt.plot(batch_y, label="Actual Data")
+plt.legend()
+plt.show()
