0b57475c1350beae3d9cf0beec752f5d974cb790
Thomas Power <tpaschalpower@gmail.com>
2018-08-09 21:02:41 +0100

    Add files via upload

--- Tensorflow_Speculator.py
+++ Tensorflow_Speculator.py
@@ -28,18 +28,19 @@ batch_y = np.zeros([(batch_size)], dtype=float)
 
 #Create batch_pool. Data to be fed into neural network
 def createBatch(gen):
-	
+	#Select random file from training data
 	cur_string = currencies[random.randint(0, (len(currencies)-1))]
 	data_files = os.listdir(r"./import/" + cur_string + "/")
-	
 	num = random.randint(1, (len(data_files)-1))
 	
-	Source_Path = "./import/" + cur_string + "/" + data_files[num]
-	Scale_Path = "./import/" + cur_string + "/" + data_files[num-1]
 	#import csv data as dataframe
+	Source_Path = "./import/" + cur_string + "/" + data_files[num]
 	Source_Data = pd.read_csv(Source_Path, header=0)
 	
+	#import previous periods data for scaling
 	Scale_Data = pd.read_csv(Scale_Path, header=0)
+	Scale_Path = "./import/" + cur_string + "/" + data_files[num-1]
+	
 	#Isolate opening value column
 	Source_Data = Source_Data["open"]
 	Scale_Data = Scale_Data["open"]
@@ -57,10 +58,10 @@ def createBatch(gen):
 		gen_num = gen
 		data_range = block_size * gen_num
 		max_range = (len(Source_Data) - (12 * max_generations)) - 1
-		T = random.randint(data_range, max_range)				# pick random point for T
-		T_Vector = Source_Data[T]								# Get vector for T
-		Hist_Range = Source_Data[(T-data_range) : T]			# gather 30 points per generation before T as Historical data
-		Hist_Price = Hist_Range[0::gen_num]						# isolate 30 points within that range
+		T = random.randint(data_range, max_range)									# pick random point for T
+		T_Vector = Source_Data[T]													# Get vector for T
+		Hist_Range = Source_Data[(T-data_range) : T]								# gather 30 points per generation before T as Historical data
+		Hist_Price = Hist_Range[0::gen_num]											# isolate 30 points within that range
 		Target = Source_Data[T + (gen_num * 6 + ( random.randint(0, 6)))]			# randomly pick T + [6-12] as target value
 		for e in range(block_size):
 			Projection_Value[e] = Hist_Price[e]
@@ -79,7 +80,7 @@ M = 256
 N = 128
 O = 64
 
-X = tf.placeholder(tf.float32, [None, block_size], name='InputData')	 	 # input shape 30
+X = tf.placeholder(tf.float32, [None, block_size], name='InputData') # input shape 30
 Y = tf.placeholder(tf.float32, [None, 1], name='LabelData')
 
 W1 = tf.Variable(tf.truncated_normal([block_size, L], stddev=0.1)) 	 # random weights for the hidden layer 1
@@ -111,7 +112,7 @@ summary_op = tf.summary.merge_all()
 init_op = tf.global_variables_initializer()
 
 saver = tf.train.Saver()
-for generation in range(1, 6):
+for generation in range(1, max_generations+1):
 	with tf.Session() as sess:
 		sess.run(init_op)																			# run the initializer
 		writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph()) 					# create writer for accuracy logs
