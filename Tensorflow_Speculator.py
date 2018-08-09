07d17b8dc5f33b1d2a9484e8b69bb38d8118d377
Thomas Power <tpaschalpower@gmail.com>
2018-08-09 20:07:22 +0100

    Add files via upload

--- Tensorflow_Speculator.py
+++ Tensorflow_Speculator.py
@@ -9,50 +9,60 @@ import os
 import time
 
 #locate Source_Data
-data_files = os.listdir(r"./Import_Data")
+currencies = os.listdir(r"./import/")
 
 logs_path = 'log_relu/'
-batch_count = 10
-learning_rate = 0.00003
+batch_size = 10
+learning_rate = 0.000003
 training_epochs = 10000
 display_epoch = 1
 
-batch_size = 30
+block_size = 30
 max_generations = 5
 
 
 #create arrays to fill with feed data
-Projection_Value = np.zeros([batch_size], dtype=float)
-Hist_Source = np.zeros([batch_size], dtype=float)
-batch_x = np.zeros([(batch_count), batch_size], dtype=float)
-batch_y = np.zeros([(batch_count)], dtype=float)
+Projection_Value = np.zeros([block_size], dtype=float)
+batch_x = np.zeros([(batch_size), block_size], dtype=float)
+batch_y = np.zeros([(batch_size)], dtype=float)
 
 #Create batch_pool. Data to be fed into neural network
 def createBatch(gen):
-	Source_Path = "./Import_Data/" + data_files[random.randint(0, (len(data_files)-1))]
 	
+	cur_string = currencies[random.randint(0, (len(currencies)-1))]
+	data_files = os.listdir(r"./import/" + cur_string + "/")
+	
+	num = random.randint(1, (len(data_files)-1))
+	
+	Source_Path = "./import/" + cur_string + "/" + data_files[num]
+	Scale_Path = "./import/" + cur_string + "/" + data_files[num-1]
 	#import csv data as dataframe
 	Source_Data = pd.read_csv(Source_Path, header=0)
 	
+	Scale_Data = pd.read_csv(Scale_Path, header=0)
 	#Isolate opening value column
-	Source_Data = Source_Data["Open"]
+	Source_Data = Source_Data["open"]
+	Scale_Data = Scale_Data["open"]
 	
 	#scale data between -1 and 1
 	scaler = MinMaxScaler(feature_range=(-1, 1))
 	Source_Data = np.reshape(Source_Data, (-1, 1))
-	scaler.fit(Source_Data)
+	scale_mean = Scale_Data.mean()
+	scale = [scale_mean * 0.97, scale_mean * 1.03]
+	scale = np.reshape(scale, (-1, 1))
+	scaler.fit(scale)
 	Source_Data = scaler.transform(Source_Data)
 
-	for i in range(batch_count):
+	for i in range(batch_size):
 		gen_num = gen
-		data_range = batch_size * gen_num
+		data_range = block_size * gen_num
 		max_range = (len(Source_Data) - (12 * max_generations)) - 1
 		T = random.randint(data_range, max_range)				# pick random point for T
 		T_Vector = Source_Data[T]								# Get vector for T
 		Hist_Range = Source_Data[(T-data_range) : T]			# gather 30 points per generation before T as Historical data
 		Hist_Price = Hist_Range[0::gen_num]						# isolate 30 points within that range
 		Target = Source_Data[T + (gen_num * 6 + ( random.randint(0, 6)))]			# randomly pick T + [6-12] as target value
-		for e in range(batch_size):
+		for e in range(block_size):
 			Projection_Value[e] = Hist_Price[e]
 
 		batch_x[i] = Projection_Value							#projection data and target now defined as batch_pool[i]
@@ -60,6 +70,8 @@ def createBatch(gen):
 
 	
 batch_y = np.reshape(batch_y, (-1, 1))
+
+
 # Define Neurons per layer
 
 L = 768
@@ -67,10 +79,10 @@ M = 256
 N = 128
 O = 64
 
-X = tf.placeholder(tf.float32, [None, batch_size], name='InputData')	 	 # input shape 30*2 value change and time distance
+X = tf.placeholder(tf.float32, [None, block_size], name='InputData')	 	 # input shape 30
 Y = tf.placeholder(tf.float32, [None, 1], name='LabelData')
 
-W1 = tf.Variable(tf.truncated_normal([batch_size, L], stddev=0.1)) 	 # random weights for the hidden layer 1
+W1 = tf.Variable(tf.truncated_normal([block_size, L], stddev=0.1)) 	 # random weights for the hidden layer 1
 B1 = tf.Variable(tf.ones([L])) 									 	 # bias vector for layer 1
 Y1 = tf.nn.relu(tf.matmul(X, W1) + B1)  							 # Output from layer 1
 
@@ -99,17 +111,16 @@ summary_op = tf.summary.merge_all()
 init_op = tf.global_variables_initializer()
 
 saver = tf.train.Saver()
-for generation in [3, 5]:
+for generation in range(1, 6):
 	with tf.Session() as sess:
 		sess.run(init_op)																			# run the initializer
 		writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph()) 					# create writer for accuracy logs
 		for epoch in range(training_epochs):
-			createBatch(generation)																			# prepare data for input
-			f = batch_count * epoch																	#separates data by epoch
-			for i in range(batch_count):
+			createBatch(generation)																			# prepare data for input																	#separates data by epoch
+			for i in range(batch_size):
 				sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})						 		#train data
 				_,summary = sess.run([train_op, summary_op], feed_dict={X: batch_x, Y: batch_y})	#produce summary
-				writer.add_summary(summary, epoch * batch_count + (i+f))							#record summary
+				writer.add_summary(summary, epoch * batch_size + i)									#record summary
 			print("Generation:", generation)
 			print("Epoch: ", epoch)
 			print("Optimization Finished!")
